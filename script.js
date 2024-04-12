651c4d27cbed626d42a7213954beff7682665ca0
Olofin2791 <156848564+Olofin2791@users.noreply.github.com>
2024-04-12 19:22:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const morebutton = document.getElementById("more");
+
+morebutton.addEventListener("click", function (e) {
+  // console.log("thank you");
+  document.body.innerHTML = "<h3> THANK YOU </h3>";
+});
